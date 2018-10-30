9ff1a2d08c9fd84c1e73c5c055cbfaf5a7bf14b9
Raghav Suryanarayanan <Raghav.suryanarayanan@bglgroup.co.uk>
2018-10-30 21:46:00 +0000

    RS - Generalising the KNN Algorithm

--- plinko/score.js
+++ plinko/score.js
@@ -19,13 +19,22 @@ const runAnalysis = (array) => {
             .map(row => [distance(row[0]), row[3]])
             .sortBy(row => row[0])
             .slice(0, k)
-            .countBy(row => row[1])
+            .countBy(row => row[1]) //{ "1": 2, "4": 3 }
             .toPairs()
             .sortBy(row => row[1])
             .last()
             .first()
             .parseInt()
             .value();
+}
+
+const splitDataSet = (data, testCount) => {
+    const shuffledData = _.shuffle(data);
+
+    const testData = _.slice(shuffledData, 0, testCount);
+    const trainingData = _.slice(shuffledData, testCount);
+
+    return [testData, trainingData];
 }
 
 export { onScoreUpdated, runAnalysis };
